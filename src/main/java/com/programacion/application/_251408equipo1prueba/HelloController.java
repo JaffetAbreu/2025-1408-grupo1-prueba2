de5a08a5565c0c3b7f14b5631903cee76f21cae3
Brandon Cueva <brandon_cueva@unitec.edu>
2025-09-08 20:28:50 -0600

    Initial commit

---
+++ src/main/java/com/programacion/application/_251408equipo1prueba/HelloController.java
@@ -0,0 +1,14 @@
+package com.programacion.application._251408equipo1prueba;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.programacion.application._251408equipo1prueba {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.programacion.application._251408equipo1prueba to javafx.fxml;
+    exports com.programacion.application._251408equipo1prueba;
+}
